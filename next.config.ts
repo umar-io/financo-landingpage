36a972abbdf204ab8debbc6717dc1591c8692edb
Umar Irale <mailpersonal725@gmail.com>
2025-09-18 08:37:38 -0700

    Removed Middleware

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.[^/]+$).*)",
+  ],
 };
 
 export default nextConfig;
